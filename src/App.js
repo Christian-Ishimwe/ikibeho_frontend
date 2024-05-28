8df68167df97f571c9266b7232acb7afa5df41f0
christian Ishimwe <christianinja3@gmail.com>
2024-05-28 08:32:34 +0200

    Added the first Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import MyIntro from './MyIntro';
+import MyProjects from './MyProjects';
 
 function App() {
   return (
     <div className="App">
-        <header className="App-header">
-        <h1>IKIBEHO DIGITAL FOUNDATION</h1>
-      </header>
+      <MyIntro/>
+      <MyProjects/>
     </div>
   );
 }
---
+++ src/MyIntro.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+const MyIntro=()=>{
+    return (
+        <>
+            <h2>Welcome.</h2>
+            <p>
+                My name is Christian ISHIMWE,   I am a fullstack developer 
+                based in Kigali, Rwanda. I have developed many projects from
+                 ecommerce platforms to Payment Platforms.
+            </p>
+            <p>
+                I'm passionate about cutting-edge cloud compution, robust, secure and 
+                Fast applications.
+            </p>
+        </>
+    )
+}
+
+
+export default MyIntro
