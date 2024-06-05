3ddbf5b7edc5d64c3de25a18ed866e2f4959c4d0
christian Ishimwe <christianinja3@gmail.com>
2024-06-05 09:11:43 +0200

    Added the progress Bar

---
+++ src/Progress.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./progress.css"
+const Progress=()=>{
+    return (
+        <>
+            <div className="progress" >
+               
+            </div>
+        </>
+    )
+}
+
+export default Progress
+
